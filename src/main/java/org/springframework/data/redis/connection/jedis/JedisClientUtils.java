602f2e55517a32b267714e53733db4bbaa95eaf8
Tihomir Mateev <tihomir.mateev@gmail.com>
2026-02-10 16:47:20 +0200

    Reverting unnecessary modifications
    Signed-off-by: Tihomir Mateev <tihomir.mateev@gmail.com>

--- src/main/java/org/springframework/data/redis/connection/jedis/JedisClientUtils.java
+++ src/main/java/org/springframework/data/redis/connection/jedis/JedisClientUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2026-present the original author or authors.
+ * Copyright 2017-present the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ import java.util.stream.Collectors;
  * @author Serhii Siryi
  * @since 2.1
  */
-@SuppressWarnings({"ConstantConditions" })
+@SuppressWarnings({ "unchecked", "ConstantConditions" })
 class JedisClientUtils {
 
 	private static final Set<String> KNOWN_COMMANDS;
